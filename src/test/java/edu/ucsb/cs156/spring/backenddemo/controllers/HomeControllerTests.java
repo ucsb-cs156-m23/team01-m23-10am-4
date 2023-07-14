c524f28e0059b530d431ee65ec05c9c42a132c21
gento <41165204+dscpsyl@users.noreply.github.com>
2023-07-14 01:42:55 -0700

    DS - ADD: homecontroller tests

--- src/test/java/edu/ucsb/cs156/spring/backenddemo/controllers/HomeControllerTests.java
+++ src/test/java/edu/ucsb/cs156/spring/backenddemo/controllers/HomeControllerTests.java
@@ -49,12 +49,13 @@ public class HomeControllerTests {
         String baseUrl = "http://localhost:8080/";
 
         List<String> team = new ArrayList<String>();
-        team.add("Jing P.");
-        team.add("Christian S.");
-        team.add("Guy W.");
-        team.add("Phill C.");
+        team.add("Yuanchen M.");
+        team.add("Qi S.");
+        team.add("Iain W.");
+        team.add("David S.");
+        team.add("Matt P.");
         resultMap.put("team",team);
-        resultMap.put("repo","https://github.com/ucsb-cs156-m23/STARTER-team01");
+        resultMap.put("repo","https://github.com/ucsb-cs156-m23/team01-m23-10am-4");
         resultMap.put("api-documentation", baseUrl + "swagger-ui/index.html");
         String expected = mapper.writeValueAsString(resultMap);
         String actual = HomeController.getHomePageObjectJSON("http://localhost:8080/");
